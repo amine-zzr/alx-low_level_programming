ec420c5cd9906353a05d9bfd368873e6994f5c76
amine-zzr <aminezoukri@gmail.com>
2023-11-21 14:09:03 +0000

    modified 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *ptr;
+	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
-	temp = *head;
-	while (temp != NULL)
+	while (*head != NULL)
 	{
-		ptr = temp->next;
-		free(temp);
-		temp = ptr;
+		temp = haed;
+		free(*head);
+		*head = temp->next;
 	}
 
 	*head = NULL;
