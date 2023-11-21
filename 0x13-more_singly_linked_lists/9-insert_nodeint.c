8d4ccc7847086e2ff3d861eb453b30fa2b6ba0d4
amine-zzr <aminezoukri@gmail.com>
2023-11-21 16:47:27 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		ptr = ptr->next;
 	}
 
-	if (counter > idx)
+	if (counter != idx)
 		return (NULL);
 
 	new->next = ptr->next;
