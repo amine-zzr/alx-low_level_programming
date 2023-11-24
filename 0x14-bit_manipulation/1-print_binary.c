775633253aed6544ceb8d5d62145868e3b829fc0
amine-zzr <aminezoukri@gmail.com>
2023-11-24 16:26:17 +0000

    using recursion in task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,37 +1,16 @@
 #include "main.h"
+
 /**
- * print_binary - print binary
- * @n: integer to mess with
+ * print_binary - prints the binary represantation of a number.
+ * @n: the nubmer to print.
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 1;
+	/* using recursion to shift the bits to the right*/
+	if (n > 1)
+		print_binary(n >> 1);
 
-	i <<= ((sizeof(i) * 8) - 1);
-	if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-		}
-	while (i > 0)
-	{
-		if ((i & n) == 0)
-			i = i >> 1;
-		else
-			break;
-	}
-	while (i > 0)
-	{
-		if ((i & n) == 0)
-			_putchar('0');
-		else
-			_putchar('1');
-		i = i >> 1;
-	}
-
+	/* using the bitwise AND to ectract the first bit*/
+	_putchar((n & 1) + '0');
 }
