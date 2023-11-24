9bd73f1c41072e708d7954fd54024677b86ba2cd
amine-zzr <aminezoukri@gmail.com>
2023-11-24 00:23:18 +0000

    modified 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,3 +14,17 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (1);
 	return (-1);
 }
+
+/**
+ * get_bit - gets bit a index
+ * @n: number to go through
+ * @index: index to go through
+ * Return: int value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+
+	return (n >> index & 1);
+}
