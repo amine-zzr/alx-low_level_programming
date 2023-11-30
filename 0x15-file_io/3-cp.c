3a6a602cdb2635eeb19ca7268c0acb594c5fc87a
amine-zzr <aminezoukri@gmail.com>
2023-11-30 01:00:50 +0000

    erro hundled

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define SIZE 1024
 
 /**
  * main - copies the content of a file to another file.
@@ -11,7 +10,7 @@
 int main(int argc, char **argv)
 {
 	int fd1, fd2;
-	char buffer[SIZE];
+	char buffer[1024];
 	ssize_t readed, written;
 
 	if (argc != 3)
@@ -22,14 +21,14 @@ int main(int argc, char **argv)
 
 	fd1 = open(argv[1], O_RDONLY);
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	readed = read(fd1, buffer, SIZE);
+	readed = read(fd1, buffer, 1024);
 
 	if (fd1 == -1 || readed == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	while ((readed = read(fd1, buffer, SIZE)) > 0)
+	while (readed > 0)
 	{
 		written = write(fd2, buffer, readed);
 		if (fd2 == -1 || written == -1)
@@ -37,6 +36,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
+		readed = read(fd1, buffer, 1024);
 	}
 	if (close(fd1) == -1)
 	{
@@ -48,6 +48,5 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
-
 	return (0);
 }
