e5bd49c37b73774ba60cb9aa2f41b2bee6837d23
amine-zzr <aminezoukri@gmail.com>
2024-01-11 20:43:37 +0000

    Betty style fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints a doubly linked list elements.
+ * @h: the head of the list.
+ * Return: number of nodes.
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t counter = 0;
