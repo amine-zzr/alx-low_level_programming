a56ad57b8a8a442187144d773a00f0136bc731b7
amine-zzr <aminezoukri@gmail.com>
2024-02-03 19:04:24 +0000

    allocatting memory for the hash table structure and the array of linked lists

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: A pointer to the newly created hash table
+ * or NULL if an error occurs
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL;
+	unsigned long int i;
+
+	if (size < 1)
+		return (NULL);
+
+	/* allocating memory fot the hash table */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	/* allocate memory for the array of hash_node_t pointers */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	/* initialize each element to NULL */
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
+	new_table->size = size;
+
+	return (new_table);
+}
